ffbe26a355c72fabff0a119ec82d9e2845e809a1
Ezequiel Rosas <ezequiel.rosas@autodesk.com>
2017-01-08 20:10:17 -0600

    Add failing test

---
+++ src/test/java/mx/net/nhtzr/base/Service/AsyncNothingServiceTest.java
@@ -0,0 +1,15 @@
+package mx.net.nhtzr.base.Service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by e.rosas.garcia on 08/01/2017.
+ */
+public class AsyncNothingServiceTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(false);
+    }
+}
